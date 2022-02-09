adb94de85f4b02861581355bbea7f5e04c804226
PavelKarmazin <karmazind1@gmail.com>
2022-02-09 13:36:38 +0200

    Взрыв блоков

--- Tanki2D.py
+++ Tanki2D.py
@@ -6,36 +6,32 @@ pygame.init()
 sc = pygame.display.set_mode((1200,900))
 pygame.display.set_caption("Танки")
 pygame.display.flip()
-L8 = [4, 7]
-y8 = 600-23*6
-L7 = [4, 7]
-y7 = 500-23*5
-L6 = [2, 3, 4, 7, 8, 9]
-y6 = 700-23*7
+### списки содержат числа для генерации блоков по оси Х
+y0, y1, y2, y3, y4, y5, y6, y7, y8 = 0, 100, 200, 300, 400, 500, 600, 700, 800 #высоты карты для создания блоков
+L8 = []
+L7 = [0, 1, 2, 3, 4, 7, 8, 9, 10, 11]
+L6 = [4, 7]
 L5 = [2, 3, 4, 7, 8, 9]
-y5 = 400-23*4
-L4 = [3, 4, 5, 6, 7, 8]
-y4 = 900-20*9
-L3 = [4, 7]
-y3 = 300-23*3
+L4 = []
+L3 = [2, 3, 4, 7, 8, 9]
 L2 = [4, 7]
-y2 = 200-23*2
 L1 = [0, 1, 2, 3, 4, 7, 8, 9, 10, 11]
-y1 = 100
-#### y это высота для блоков кирпичей в определенной стене
-gunX = 0
-gunY = 0
+L0 = []
 goUP = False
 goDW = False
 goRT = False
 goLT = False
 
-gunCount = 0
+gunCount = 0 ### перезарядка оружия
 
 upStop = False
 dwStop = False
 rtStop = False
 ltStop = False
+#активация стопа при встрече с препятсвиями
+ltlt = False
+rtrt = False
+# вспомогают в def
 
 WHITE = (255, 255, 255)
 RED = (225, 0, 50)
@@ -44,9 +40,13 @@ BLUE = (0, 0, 225)
 BLACK = (0,0,0)
 GREY = (128,128,128)
 YELLOW = (255, 255, 0)
+#стандартные цвета
 corXU, corXD, corXL, corXR = [], [], [], []
 corYU, corYD, corYL, corYR = [], [], [], []
 copyU, copyD, copyL, copyR = [], [], [], []
+# списки с координатами снарядов
+clock = pygame.time.Clock()
+fps = clock.tick(30)
 
 x = 550
 y = 800
@@ -54,7 +54,10 @@ tankUp = True
 tankR = False
 tankL = False
 tankD = False
+### переменные go содержат текущее нарпавление танка в виде bool
 game_over = False
+provk1 = False
+provk2 = False
 block = pygame.image.load(r'F:\PyGame\png\block.png')
 tank = pygame.image.load(r'F:\PyGame\png\tank.png')
 tank1 = pygame.image.load(r'F:\PyGame\png\tank1.png')
@@ -64,6 +67,76 @@ snaryad = pygame.image.load(r'F:\PyGame\png\snaryad.png')
 snaryadR = pygame.image.load(r'F:\PyGame\png\snaryadR.png')
 snaryadD = pygame.image.load(r'F:\PyGame\png\snaryadD.png')
 snaryadL = pygame.image.load(r'F:\PyGame\png\snaryadL.png')
+
+def boom(I, Y, l, a):
+    global provk1, provk2
+    if a<100:
+        a = "0"
+    elif a>1000:
+        a = str(a)
+        a = a[:2]
+    else:
+        a = str(a)
+        a = a[0]
+    if I==Y+33 or I==Y+34 or I==Y+35 or I==Y+36:
+        provk1 = True
+    else:
+        provk1 = False
+    if int(a) in l:
+        provk2 = True
+    else:
+        provk2 = False
+    if provk1 and provk2:
+        l.remove(int(a))
+        return True
+# это был выстрел вверх/вниз 
+def boom1(I, Y, l, a):
+    global provk1, provk2
+    if I in range(Y, Y+100) or I+14 in range(Y, Y+100):
+        provk1 = True
+    else:
+        provk1 = False
+    if a<100:
+        a = "0"
+    elif a>1000:
+        a = str(a)
+        a = a[:2]
+    else:
+        a = str(a)
+        a = a[0]
+    if int(a) in l:
+        provk2 = True
+    else:
+        provk2 = False
+    if provk1 and provk2:
+        l.remove(int(a))
+        return True
+# выстрел в лево
+
+def boom2(I, Y, l, a):
+    global provk1, provk2
+    if I in range(Y, Y+95) or I+14 in range(Y, Y+95):
+        provk1 = True
+    else:
+        provk1 = False
+    a = a-40
+    if a<100:
+        a = "0"
+    elif a>1000:
+        a = str(a)
+        a = a[:2]
+    else:
+        a = str(a)
+        a = a[0]
+    if int(a)+1 in l:
+        provk2 = True
+    else:
+        provk2 = False
+    if provk1 and provk2:
+        l.remove(int(a)+1)
+        return True
+# выстрел в право
+
 def nazvanie(stop, Y, l):
     if x<100:
         a = "0"
@@ -77,7 +150,7 @@ def nazvanie(stop, Y, l):
         prov1 = True
     else:
         prov1 = False
-    if y in range(Y, Y+77):
+    if y in range(Y, Y+97):
         prov2 = True
     else:
         prov2 = False
@@ -100,7 +173,7 @@ def nazvanie2(stop, Y, l):
         prov1 = True
     else:
         prov1 = False
-    if y+100 in range(Y, Y+75):
+    if y+100 in range(Y, Y+97):
         prov2 = True
     else:
         prov2 = False
@@ -110,19 +183,18 @@ def nazvanie2(stop, Y, l):
         stop = False
     return stop
 
-def nazvanie3(stop, Y, l):
-    global x
-    if y in range(Y, Y+75) or y+75 in range(Y, Y+75):
+def nazvanie3(stop, Y, l, Y1, X):
+    if Y1 in range(Y, Y+97) or Y1+100 in range(Y, Y+100):
         prov1 = True
     else:
         prov1 = False
-    if x<100:
+    if X<100:
         a = "0"
-    elif x>1000:
-        a = str(x)
+    elif X>1000:
+        a = str(X)
         a = a[:2]
     else:
-        a = str(x)
+        a = str(X)
         a = a[0]
     if int(a) in l:
         prov2 = True
@@ -130,14 +202,12 @@ def nazvanie3(stop, Y, l):
         prov2 = False
     if prov1 and prov2:
         stop = True
-        x+=1
     else:
-        stop = False    
+        stop = False  
     return stop
 
 def nazvanie4(stop, Y, l):
-    global x
-    if y in range(Y, Y+75) or y+75 in range(Y, Y+75):
+    if y in range(Y, Y+97) or y+100 in range(Y, Y+100):
         prov1 = True
     else:
         prov1 = False
@@ -155,12 +225,12 @@ def nazvanie4(stop, Y, l):
         prov2 = False
     if prov1 and prov2:
         stop = True
-        x-=1
     else:
         stop = False    
     return stop
-
+### эти 4 функции это стоп танк, проверяют на препятсвия
 while True:
+    clock.tick(fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -182,46 +252,90 @@ while True:
         sc.blit(block, [i*100,y7])
     for i in L8:
         sc.blit(block, [i*100,y8])
-    if nazvanie(upStop, y1, L1) or nazvanie(upStop, y2, L2) or nazvanie(upStop, y3, L3) or nazvanie(upStop, y4, L4) or nazvanie(upStop, y5, L5) or nazvanie(upStop, y6, L6) or nazvanie(upStop, y7, L7) or nazvanie(upStop, y8, L8):
-        upStop = True
-    else:
-        upStop = False
-    if nazvanie2(dwStop, y1, L1) or nazvanie2(dwStop, y2, L2) or nazvanie2(dwStop, y3, L3) or nazvanie2(dwStop, y4, L4) or nazvanie2(dwStop, y5, L5) or nazvanie2(dwStop, y6, L6) or nazvanie2(dwStop, y7, L7) or nazvanie2(dwStop, y8, L8):
-        dwStop = True
-    else:
-        dwStop = False
-    if nazvanie3(ltStop, y1, L1) or nazvanie3(ltStop, y2, L2) or nazvanie3(ltStop, y3, L3) or nazvanie3(ltStop, y4, L4) or nazvanie3(ltStop, y5, L5) or nazvanie3(ltStop, y6, L6) or nazvanie3(ltStop, y7, L7) or nazvanie3(ltStop, y8, L8):
-        ltStop = True
-    else:
-        ltStop = False
-    if nazvanie4(rtStop, y1, L1) or nazvanie4(rtStop, y2, L2) or nazvanie4(rtStop, y3, L3) or nazvanie4(rtStop, y4, L4) or nazvanie4(rtStop, y5, L5) or nazvanie4(rtStop, y6, L6) or nazvanie4(rtStop, y7, L7) or nazvanie4(rtStop, y8, L8):
-        rtStop = True
-    else:
-        rtStop = False
+    # генерация по спискам Л и высотам У
+    upStop = False
+    if y in range(y0, y2+1):
+            if nazvanie(upStop, y1, L1) or nazvanie(upStop, y2, L2):
+                upStop = True
+    if y in range(y2, y4+1):
+            if nazvanie(upStop, y3, L3) or nazvanie(upStop, y4, L4):
+                upStop = True
+    if y in range(y4, y6+1):
+            if nazvanie(upStop, y5, L5) or nazvanie(upStop, y6, L6):
+                upStop = True
+    if y in range(y6, 901):
+            if nazvanie(upStop, y7, L7) or nazvanie(upStop, y8, L8):
+                upStop = True
+    # в зависимости от положения танка проверяю на препяствия
+    
 
+    dwStop = False
+    if y in range(y0, y2+1):
+            if nazvanie2(dwStop, y1, L1) or nazvanie2(dwStop, y2, L2):
+                dwStop = True
+    if y in range(y2, y4+1):
+            if nazvanie2(dwStop, y3, L3) or nazvanie2(dwStop, y4, L4):
+                dwStop = True
+    if y in range(y4, y6+1):
+            if nazvanie2(dwStop, y5, L5) or nazvanie2(dwStop, y6, L6):
+                dwStop = True
+    if y in range(y6, 901):
+            if nazvanie2(dwStop, y7, L7) or nazvanie2(dwStop, y8, L8):
+                dwStop = True
+        
+
+    ltStop = False
+    if y in range(y0, y2+1):
+            if nazvanie3(ltStop, y1, L1, y, x) or nazvanie3(ltStop, y2, L2, y, x):
+                ltStop = True
+    if y in range(y2, y4+1):
+            if nazvanie3(ltStop, y3, L3, y, x) or nazvanie3(ltStop, y4, L4, y, x):
+                ltStop = True
+    if y in range(y4, y6+1):
+            if nazvanie3(ltStop, y5, L5, y, x) or nazvanie3(ltStop, y6, L6, y, x):
+                ltStop = True
+    if y in range(y6, 901):
+            if nazvanie3(ltStop, y7, L7, y, x) or nazvanie3(ltStop, y8, L8, y, x):
+                ltStop = True
+        
+
+    rtStop = False
+    if y in range(y0, y2+1):
+            if nazvanie4(rtStop, y1, L1) or nazvanie4(rtStop, y2, L2):
+                rtStop = True
+    if y in range(y2, y4+1):
+            if nazvanie4(rtStop, y3, L3) or nazvanie4(rtStop, y4, L4):
+                rtStop = True
+    if y in range(y4, y6+1):
+            if nazvanie4(rtStop, y5, L5) or nazvanie4(rtStop, y6, L6):
+                rtStop = True
+    if y in range(y6, 901):
+            if nazvanie4(rtStop, y7, L7) or nazvanie4(rtStop, y8, L8):
+                rtStop = True
 
 
     keys = pygame.key.get_pressed()
     if tankUp:
-        sc.blit(tank, [x, y])
+        sc.blit(tank, [x, y])# если танк прямо то рисуем соответстующий
         if keys[pygame.K_SPACE]:
             if gunCount >= 40:
                 gunCount = 0
-                corXU.append(x+25)
-                corYU.append(y-45)
+                corXU.append(x+42)# добавляем в список координат снарядов соответстующие значения, которые позже отрисуем
+                corYU.append(y)
+ 
     elif tankR:
         sc.blit(tank1, [x, y])
         if keys[pygame.K_SPACE]:
             if gunCount >= 40:
                 gunCount = 0
                 corXR.append(x+100)
-                corYR.append(y+25)
+                corYR.append(y+42)
     elif tankD:
         sc.blit(tank2, [x, y])
         if keys[pygame.K_SPACE]:
             if gunCount >= 40:
                 gunCount = 0
-                corXD.append(x+25)
+                corXD.append(x+42)
                 corYD.append(y+100)
     elif tankL:
         sc.blit(tank3, [x, y])
@@ -229,32 +343,51 @@ while True:
             if gunCount >= 40:
                 gunCount = 0
                 corXL.append(x-45)
-                corYL.append(y+25)
+                corYL.append(y+42)
 
     if keys[pygame.K_LEFT]:
         if x>0 and not ltStop:
-            x -= 3
+            x -= 3 # нажал на лево - едешь на лево
             tankUp, tankR, tankL, tankD = False, False, True, False
+        if upStop:
+            y += 3
+        if dwStop:
+            y -= 3
     if keys[pygame.K_RIGHT]:
         if x<1110 and not rtStop:
             x += 3
             tankUp, tankR, tankL, tankD = False, True, False, False
+        if upStop:
+            y += 3
+        if dwStop:
+            y -= 3
     if keys[pygame.K_UP]:
         if y>0 and not upStop:
             y -= 3
             tankUp, tankR, tankL, tankD = True, False, False, False
+        if ltStop:
+            x += 3
+        if rtStop:
+            x -= 3
     if keys[pygame.K_DOWN]:
         if y<800 and not dwStop:
             y += 3
             tankUp, tankR, tankL, tankD = False, False, False, True
+        if ltStop:
+            x += 3
+        if rtStop:
+            x -= 3
 
-    for i, j in enumerate(corYU):
-        sc.blit(snaryad, [corXU[i], j])
+    for i, j in enumerate(corYU): # магия, это не эльфийский
+        sc.blit(snaryad, [corXU[i], j]) # берем список со снарядами которые летят вверх и рисуем все эти снаряды
+        if boom(j, y1, L1, corXU[i]) or boom(j, y2, L2, corXU[i]) or boom(j, y3, L3, corXU[i]) or boom(j, y4, L4, corXU[i]) or boom(j, y5, L5, corXU[i]) or boom(j, y6, L6, corXU[i]) or boom(j, y7, L7, corXU[i]) or boom(j, y8, L8, corXU[i]) or boom(j, y0, L0, corXU[i]):
+           corYU.pop(i)
+           corXU.pop(i) # если снаряд врезался в один из блоков, то удаляем этот снаряд из списков 
     for j in corYU:
         copyU.append(j-4)
     corYU = []
     corYU = copyU.copy()
-    copyU.clear()
+    copyU.clear()## смещаем все снаряды которые летят вверх на 4 пикселя, это их скорость
     for d, i in enumerate(corYU):
         if i > 0:
             copyU.append(i)
@@ -263,11 +396,15 @@ while True:
     corYU.clear()
     corYU = copyU.copy()
     copyU.clear()
+    # перезапись всех снарядов, те что улетели вверх за границу карты не перезаписываем
 
-    ###
+    ### 
 
-    for i, j in enumerate(corYD):
+    for i, j in enumerate(corYD): # аналогично с верхом
         sc.blit(snaryadD, [corXD[i], j])
+        if boom(j, y1, L1, corXD[i]) or boom(j, y2, L2, corXD[i]) or boom(j, y3, L3, corXD[i]) or boom(j, y4, L4, corXD[i]) or boom(j, y5, L5, corXD[i]) or boom(j, y6, L6, corXD[i]) or boom(j, y7, L7, corXD[i]) or boom(j, y8, L8, corXD[i]) or boom(j, y0, L0, corXD[i]):
+           corYD.pop(i)
+           corXD.pop(i) 
     for j in corYD:
         copyD.append(j+4)
     corYD = []
@@ -286,6 +423,9 @@ while True:
 
     for i, j in enumerate(corYL):
         sc.blit(snaryadL, [corXL[i], j])
+        if boom1(j, y1, L1, corXL[i]) or boom1(j, y2, L2, corXL[i]) or boom1(j, y3, L3, corXL[i]) or boom1(j, y4, L4, corXL[i]) or boom1(j, y5, L5, corXL[i]) or boom1(j, y6, L6, corXL[i]) or boom1(j, y7, L7, corXL[i]) or boom1(j, y8, L8, corXL[i]) or boom1(j, y0, L0, corXL[i]):
+           corYL.pop(i)
+           corXL.pop(i) 
     for j in corXL:
         copyL.append(j-4)
     corXL = []
@@ -304,8 +444,11 @@ while True:
 
     for i, j in enumerate(corYR):
         sc.blit(snaryadR, [corXR[i], j])
+        if boom2(j, y1, L1, corXR[i]) or boom2(j, y2, L2, corXR[i]) or boom2(j, y3, L3, corXR[i]) or boom2(j, y4, L4, corXR[i]) or boom2(j, y5, L5, corXR[i]) or boom2(j, y6, L6, corXR[i]) or boom2(j, y7, L7, corXR[i]) or boom2(j, y8, L8, corXR[i]) or boom2(j, y0, L0, corXR[i]):
+           corYR.pop(i)
+           corXR.pop(i) 
     for j in corXR:
-        copyR.append(j+4)
+       copyR.append(j+4)
     corXR = []
     corXR = copyR.copy()
     copyR.clear()
@@ -321,7 +464,7 @@ while True:
 
     pygame.display.update()
     sc.fill(BLACK)
-    pygame.time.delay(1)
+#    pygame.time.delay(1)
     gunCount += 1
 
 
